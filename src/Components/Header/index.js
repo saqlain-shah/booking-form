ab1a79cdba8d0fbd71a98a0792e4f98fc56826fc
shah <saqlainshahbaltee@gmail.com>
2022-10-08 05:00:17 +0500

    initial commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Toolbar, AppBar, Typography } from '@material-ui/core';
+
+const Header = ({ }) => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">
+          Booking Form
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
